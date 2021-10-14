54ee650294a0ddb924f79ec181fec4d7dd24424b
Omar David Rendon Bermudez <omar1551.orb@gmail.com>
2021-10-13 22:45:42 -0500

    Excel_1

---
+++ Console_1Excel/Console_1Excel/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using OfficeOpenXml;
+using System.Linq;
+
+namespace Console_1Excel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(ExcelPackage package = new  ExcelPackage())
+            {
+                using (FileStream rute = File.OpenRead(@"D:\DocumentosOmar\CUENTAS.xlsx"))
+                {
+                    package.Load(rute);
+                }
+
+                ExcelWorksheet cuentas = package.Workbook.Worksheets.First();
+
+                for (int row = 1; ; row++)
+                {
+                    string fecha = cuentas.Cells[row, 1].Text;
+
+                    if (string.IsNullOrEmpty(fecha))
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        string detalle = cuentas.Cells[row, 2].Text;
+                        string e_efectivo = cuentas.Cells[row, 3].Text;
+                        string g_efectivo = cuentas.Cells[row, 4].Text;
+                        string e_banco = cuentas.Cells[row, 5].Text;
+                        string g_banco = cuentas.Cells[row, 6].Text;
+
+                        Console.WriteLine("{0} - {1} - {2} - {3} - {4} - {5}", fecha, detalle, e_efectivo, g_efectivo, e_banco, g_banco);
+                    }
+                }
+
+                ExcelWorksheet cuentas_2 = package.Workbook.Worksheets.Last();
+                for (int row = 1; ; row++)
+                {
+                    string fecha = cuentas_2.Cells[row, 1].Text;
+
+                    if (string.IsNullOrEmpty(fecha))
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        string detalle = cuentas_2.Cells[row, 2].Text;
+
+                        Console.WriteLine("{0} - {1}", fecha, detalle);
+                    }
+                }
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
